451a7cd6195db4113edc0651f41caa8495558e1c
Dean flood <deanflood1991@gmail.com>
2017-03-22 15:14:41 +0000

    Initial commit

---
+++ DiceRoll.py
@@ -0,0 +1,8 @@
+import random
+
+print("Dice Mean: 3.5")
+mean = 0.0
+for x in range(1, 1000000):
+    mean += random.randint(1,6)
+print("Mean after 1000,000 rolls: " + str(mean / 1000000))
+mean = 0.0
