31622f330c50900797e7fe6c98eee18a769873e6
Dean flood <deanflood1991@gmail.com>
2017-03-22 15:22:15 +0000

    fixing format

--- DiceRoll.py
+++ DiceRoll.py
@@ -4,5 +4,5 @@ print("Dice Mean: 3.5")
 mean = 0.0
 for x in range(1, 1000000):
     mean += random.randint(1,6)
-print("Mean after 1000,000 rolls: " + str(mean / 1000000))
+print("Mean after 1,000,000 rolls: " + str(mean / 1000000))
 mean = 0.0
